490a306c19911687042a4894b318a4952bcc13b6
dhepilinuz <dhepilinuz@gmail.com>
2026-07-18 01:30:16 +0700

    feat: implement admin application structure with initial files and configurations

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Warung Meng</h1>
+      <p>Admin</p>
+    </div>
+  );
+}
---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+});
